f0d68ea0b467ac23a087b7ef9d666ef315b28c1d
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2018-10-24 14:05:37 +0100

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,82 @@
+// --------------------------------------
+// Includes
+// --------------------------------------
+// Libraries
+#include <SFML/Graphics.hpp>
+
+
+
+int main()
+{
+	// --------------------------------------
+	// Game Setup
+	// --------------------------------------
+
+	// Render Window creation
+	sf::RenderWindow gameWindow;
+	gameWindow.create(sf::VideoMode::getDesktopMode(), "Whack A Critter",
+		sf::Style::Titlebar | sf::Style::Close);
+
+	// Timer functionality
+	sf::Clock gameClock;
+
+	// end game setup
+	// --------------------------------------
+
+
+	// --------------------------------------
+	// Game Loop
+	// --------------------------------------
+	while (gameWindow.isOpen())
+	{
+		// --------------------------------------
+		// Input
+		// --------------------------------------
+		sf::Event event;
+		while (gameWindow.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				gameWindow.close();
+			} // End if (closed)
+		} // end event polling loop
+
+		// end input
+		// --------------------------------------
+
+
+		// --------------------------------------
+		// Update
+		// --------------------------------------
+		sf::Time frameTime = gameClock.restart();
+
+		// end update
+		// --------------------------------------
+
+
+
+		// --------------------------------------
+		// Draw
+		// --------------------------------------
+
+		// Clear the window to a single colour
+		gameWindow.clear();
+
+		// Draw Everything
+
+
+		// Display the window contents to the screen
+		gameWindow.display();
+
+		// end draw
+		// --------------------------------------
+
+	} // end of the game while loop
+
+	// end game loop
+	// --------------------------------------
+
+	// exit point for the program
+	return 0;
+
+} // end of the main() function
